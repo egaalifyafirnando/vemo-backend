f9277d5cd96b28c92f7156bb7b7d03ae7176f9f4
egaalfy <egaalifyaf@gmail.com>
2022-02-07 12:03:54 +0700

    update helpers error

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,32 +2,18 @@
 
 
 // fungsi untuk convert mata uang ke IDR
-if ( ! function_exists('money_format')) {
-
-    /*
-    * money format
-    *
-    * @param mixed $str
-    * @return void
-    */
-
-    function moneyFormat($str) {
+if (!function_exists('moneyFormat')) {
+    function moneyFormat($str)
+    {
         return 'Rp. ' . number_format($str, '0', '', '.');
     }
 }
 
 
 // fungsi untuk convert tanggal ke bahasa indonesia
-if ( ! function_exists('money_format')) {
-
-    /*
-    * money format
-    *
-    * @param mixed $tanggal
-    * @return void
-    */
-
-    function dateID($tanggal) {
+if (!function_exists('dateID')) {
+    function dateID($tanggal)
+    {
         $value = Carbon\Carbon::parse($tanggal);
         $parse = $value->locale('id');
         return $parse->translatedFormat('l, d F Y');
